59c13f2d1fa798038a544129d8fad7367bba445d
agheieff <agheieff@pm.me>
2025-04-04 20:26:07 +0200

    cleanup

--- Core/__init__.py
+++ Core/__init__.py
@@ -3,6 +3,5 @@ Core agent components
 """
 from Core.agent_runner import AgentRunner
 from Core.executor import Executor
-from Core.model_interface import ModelInterface
 
 __all__ = ['AgentRunner', 'Executor', 'ModelInterface']
